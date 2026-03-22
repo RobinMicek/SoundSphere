a80b2deaa3ded05b1dce1f32267e3e90b04e10e2
Robin Míček <micekrob@cvut.cz>
2026-03-22 17:52:00 +0100

    feat: cache app for offline mode

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { mount } from 'svelte'
 import './css/app.css'
 import App from './App.svelte'
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW();
 
 const app = mount(App, {
   target: document.getElementById('app')!,
--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,58 @@ import { defineConfig } from 'vite'
 import path from 'path';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { router } from "sv-router/vite-plugin";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     svelte(),
-    router()
+    router(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["./public/favicon.pnf"],
+
+      manifest: {
+        name: "SoundSphere",
+        short_name: "SoundSphere",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#130E0E",
+        theme_color: "#49971F",
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,svg,png,jpg}"],
+
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) => request.mode === "navigate",
+            handler: "NetworkFirst",
+            options: {
+              cacheName: "pages",
+              networkTimeoutSeconds: 3
+            }
+          },
+          {
+            urlPattern: ({ request }) => request.destination === "script" || request.destination === "style",
+            handler: "StaleWhileRevalidate",
+            options: {
+              cacheName: "assets"
+            }
+          },
+          {
+            urlPattern: ({ request }) => request.destination === "image",
+            handler: "CacheFirst",
+            options: {
+              cacheName: "images",
+              expiration: {
+                maxEntries: 100,
+                maxAgeSeconds: 60 * 60 * 24 * 30
+              }
+            }
+          }
+        ]
+      }
+    })
   ],
   
   resolve: {
