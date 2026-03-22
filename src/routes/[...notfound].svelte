8ba4a8033c0568ede55daee18805d1c850db0589
Robin Míček <micekrob@cvut.cz>
2026-03-22 17:54:55 +0100

    feat: added styling to 404 page

--- src/routes/[...notfound].svelte
+++ src/routes/[...notfound].svelte
@@ -1,1 +1,12 @@
-<h1>404 - Page Not Found</h1>
+<script>
+</script>
+
+<div class="error">
+    <p class="centered">
+        <span class="bold">404</span>
+
+        <br>
+
+        Page not found
+    </p>
+</div>
